24b36e80d27a38c7247ecb806e1822ecd1aeef4f
Siddhant Kanse <siddhantkanse17@gmail.com>
2026-07-28 20:26:45 +0530

    fire base

--- js/sales.js
+++ js/sales.js
@@ -1,4 +1,4 @@
-import { auth, db } from "./firebase-config.js";
+import { auth, db } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import {
   collection,
